d777e3aadb9bb7a66423cfd2669830a14440b5e9
sumeetswain <sumeetswain.work@gmail.com>
2023-08-28 13:40:04 +0530

    main

--- Javascript/string-methods.js
+++ Javascript/string-methods.js
@@ -1,10 +1,12 @@
 let msg = "this is a message";
 let newMsg = msg.toUpperCase();
 console.log(newMsg);
+
 let msg2 = "              this is also a message                     ";
 let newMsg2 = msg2.trim();
 console.log(newMsg2);
 //* trim removes the whitespace from start and end of a string
+
 let realEmail = "         ReAleMaIl@email.com";
 // * we can chain methods together meaning more than one method can be applied at a time
 let actualEmail = realEmail.trim().toLowerCase();
