76f55edaa24dfd9c247c50d6d4707dff9c8b0070
sumeetswain <sumeetswain.work@gmail.com>
2023-09-23 13:33:44 +0530

    main

---
+++ Javascript/eventHandling/inlineEvents.js
@@ -0,0 +1,4 @@
+const btn = document.querySelector("#btn");
+btn.onclick = function () {
+  console.log("nice clicks");
+};
---
+++ Javascript/todo-list/capitalize.js
@@ -0,0 +1,8 @@
+function capitalize(str) {
+  let newString = str.charAt(0).toUpperCase() + str.slice(1, str.length - 1);
+  //   console.log(str[(1, str.length - 1)]);
+  console.log(newString);
+  console.log(str.length);
+  console.log(str[str.length]);
+}
+capitalize("sadasdasdasd");
