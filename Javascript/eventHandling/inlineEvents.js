2ef0724e9043b27d53ce6fb90b1f9c2146257d52
sumeetswain <sumeetswain.work@gmail.com>
2023-09-23 13:52:40 +0530

    onlick property

--- Javascript/eventHandling/inlineEvents.js
+++ Javascript/eventHandling/inlineEvents.js
@@ -2,3 +2,9 @@ const btn = document.querySelector("#btn");
 btn.onclick = function () {
   console.log("nice clicks");
 };
+
+function entered() {
+  console.log("GTFO");
+}
+
+btn.onmouseenter = entered;
