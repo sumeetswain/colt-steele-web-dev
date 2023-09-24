8536d72da3f42d7dc106917bd67bd319d852d59b
sumeetswain <sumeetswain.work@gmail.com>
2023-09-24 12:49:41 +0530

    created a todo list

--- Javascript/eventHandling/preventDefault.js
+++ Javascript/eventHandling/preventDefault.js
@@ -1,5 +1,12 @@
 const form = document.querySelector("form");
+const text = document.querySelector("#text");
+const list = document.querySelector("#list");
+
 form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  console.log("submitted");
+  e.preventDefault(); // prevent submitting the page
+  const newLI = document.createElement("LI"); //create a new li element
+  newLI.innerText = text.value; // set the innertext of li from the textbox
+  list.appendChild(newLI); // add the new li to the ul
+  text.value = ""; // reset the textbox
+  // * preventDefault stops the default behaviour of the element, which in case of a form is to submit to the target page in the action attribute of form
 });
