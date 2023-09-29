fac6e444b87eed2a4b7c86df01ae48b512c9a686
sumeetswain <sumeetswain.work@gmail.com>
2023-09-29 17:13:56 +0530

    using axios

---
+++ Javascript/AJAX and APIs/axios.js
@@ -0,0 +1,23 @@
+axios
+  .get("https://swapi.dev/api/people/1/")
+  .then((res) => {
+    // console.log(res); in axios the response we get already has the data parsed inside the object
+    console.log(res.data); //we can print the data from the object by res.data
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+const getStarWars = async () => {
+  try {
+    const res = await axios.get("https://swapi.dev/api/people/10/");
+    console.log(res.data);
+  } catch (e) {
+    console.log("error", e);
+  }
+};
+getStarWars();
+
+const dadJoke = async () => {
+  const res = axios.get("https://icanhazdadjoke.com");
+};
