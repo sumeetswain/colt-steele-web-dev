a0677fb5926fceb71f389331d627bd13943547e6
sumeetswain <sumeetswain.work@gmail.com>
2023-10-02 14:21:33 +0530

    displaying all images of search result

--- Javascript/AJAX and APIs/TVShowSearch/app.js
+++ Javascript/AJAX and APIs/TVShowSearch/app.js
@@ -5,10 +5,17 @@ form.addEventListener("submit", async function (e) {
   const searchTerm = form.elements.query.value;
   const res = await axios.get(
     `https://api.tvmaze.com/search/shows?q=${searchTerm}`
-  ); // we have to wait for axios to get the result from the URL so we have to await it
-  const link = res.data[0].show.image.medium;
+  );
+  // * we have to wait for axios to get the result from the URL so we have to await it
   //* we get res as an object which has all the search results from axios and then we take the 1st result and use the show > image > medium property to get the image link
-  const img = document.createElement("img");
-  img.src = link;
-  document.body.append(img);
+  makeImages(res.data);
 });
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("img");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
