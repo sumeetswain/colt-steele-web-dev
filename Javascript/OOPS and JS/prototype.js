7b142c739729ccbc578219909e4e637411242f2c
sumeetswain <sumeetswain.work@gmail.com>
2023-10-05 19:45:15 +0530

    OOPS in Javascript

---
+++ Javascript/OOPS and JS/prototype.js
@@ -0,0 +1,6 @@
+String.prototype.yell = () => {
+  console.log("WHAT UPPP!!");
+};
+
+// * prototype is an object in which all the properties and methods of a given datatype are stored, For e.g when we use string.length method the .length method is stored in the prototype object and is present in all strings
+// * __proto__ is a reference to the prototype object which is present in all datatypes
