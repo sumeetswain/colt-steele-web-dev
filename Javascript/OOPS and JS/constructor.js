aa77a0b47b0580e6a92d8ba89fc76893a034409e
sumeetswain <sumeetswain.work@gmail.com>
2023-10-07 12:02:19 +0530

    More Classes Practice

--- Javascript/OOPS and JS/constructor.js
+++ Javascript/OOPS and JS/constructor.js
@@ -4,6 +4,7 @@ class Color {
     this.g = g;
     this.b = b;
     this.name = name;
+    this.calcHSL();
     // here by using this.r we are setting the value of color.r to the value we send while creating the new color
   }
   // the following functions are added to the prototype of the color meaning we dont have to set them for every color
@@ -27,6 +28,62 @@ class Color {
     const { r, g, b } = this;
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     // ? why cant we just send r, g and b into hex as parameters and then execute the above line??
+  }
+
+  // * More Classes Practice
+  hsl() {
+    const { h, s, l } = this;
+    return `hsl(${h},${s}%,${l}%)`;
+  }
+  opposite() {
+    const { h, s, l } = this;
+    const newHue = (h + 180) % 360;
+    return `hsl(${newHue},${s}%,${l}%)`;
+  }
+  fullySaturated() {
+    const { h, l } = this;
+    return `hsl(${h},100%,${l}%)`;
+  }
+  calcHSL() {
+    let { r, g, b } = this;
+    // Make r, g, and b fractions of 1
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+    if (delta == 0) h = 0;
+    else if (cmax == r)
+      // Red is max
+      h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+      // Green is max
+      h = (b - r) / delta + 2;
+    // Blue is max
+    else h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    // Make negative hues positive behind 360°
+    if (h < 0) h += 360;
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+    this.h = h;
+    this.s = s;
+    this.l = l;
   }
 }
 
