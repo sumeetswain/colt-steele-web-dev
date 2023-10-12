b61345baf25d0fa1a4d133129d50d0e017963a04
sumeetswain <sumeetswain.work@gmail.com>
2023-10-12 14:42:07 +0530

    using JS in EJS

--- Express.js/passing-data-to-templates.js
+++ Express.js/passing-data-to-templates.js
@@ -3,10 +3,21 @@ const app = express();
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
-app.get("/", (req, res) => {
+// using JS in ejs file and conditionals(if else)
+app.get("/random", (req, res) => {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   // we can do the math in the ejs file itself but it is preferred to do it in here
   res.render("random", { randomNumber }); // sending a variable to ejs file
+});
+// sending variables to ejs
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.render("subreddit", { subreddit }); // always send the variable in curly braces
+});
+// using for loops in ejs
+app.get("/heroes", (req, res) => {
+  const heroes = ["Mercy", "Lucio", "Reaper", "Cassidy", "Winston"];
+  res.render("heroes", { heroes });
 });
 app.listen(3000, () => {
   console.log("Listening on port 3000");
