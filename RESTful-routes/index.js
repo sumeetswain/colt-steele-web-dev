e295afdf6a04be00065fe24bcae9096aa7ff4162
sumeetswain <sumeetswain.work@gmail.com>
2023-10-16 18:41:42 +0530

    added update functionality

--- RESTful-routes/index.js
+++ RESTful-routes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuid } = require("uuid"); // package to create unique ids
+const methodOverride = require("method-override");
 app.use(express.urlencoded({ extended: true }));
 //we have to tell express to parse the data being sent to it and here we are telling it to parse the form data
 // by default the data is in text form
 app.use(express.json()); // now we are parsing json data
+app.use(methodOverride("_method")); // to send patch and delete requests from forms
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -16,17 +18,18 @@ const comments = [
   { id: uuid(), username: "Doomslayer58", comment: "Epic" },
   { id: uuid(), username: "barbieloverxoxo", comment: "Cutee" },
 ];
+// Home page
 app.get("/comments", (req, res) => {
   res.render("comments/index", { comments }); // to display all the comments(array)
 });
-
+// Creating new comments
 app.get("/comments/new", (req, res) => {
   res.render("comments/new"); // form to create a new comment
 });
-
+//post request from the form
 app.post("/comments", (req, res) => {
   const { username, comment } = req.body; //destructuring from req.body
-  comments.push({ username, comment, id }); //pushing into the comments array
+  comments.push({ username, comment, id: uuid() }); //pushing into the comments array
   // res.send("working");
   res.redirect("/comments"); // redirecting to the main page of comments after adding new comment
 });
@@ -34,6 +37,19 @@ app.get("/comments/:id", (req, res) => {
   const { id } = req.params;
   const comment = comments.find((c) => c.id === id);
   res.render("comments/show", { comment });
+});
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/edit", { comment });
+});
+
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params; // getting the id from the req
+  const newCommentText = req.body.comment; //getting the comment
+  const foundComment = comments.find((c) => c.id === id);
+  foundComment.comment = newCommentText; // redeclaring the comment text
+  res.redirect("/comments"); //redirecting to home page
 });
 app.get("/puppies", (req, res) => {
   res.send("GET /puppies response");
