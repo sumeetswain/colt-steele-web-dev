8b13c8336ae11b7cc6cbdd9c58bc9fc7eff8c94f
sumeetswain <sumeetswain.work@gmail.com>
2023-10-25 14:14:04 +0530

    Updating and Deleting using Mongoose and Express

--- Mongoose-Express/index.js
+++ Mongoose-Express/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Product = require("./models/product");
 mongoose
   .connect("mongodb://127.0.0.1:27017/farmStand")
@@ -15,13 +16,15 @@ mongoose
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
+const categories = ["fruit", "vegetable", "dairy"];
 app.get("/products", async (req, res) => {
   const products = await Product.find({});
   res.render("./products/index", { products });
 });
 app.get("/products/new", (req, res) => {
-  res.render("products/new");
+  res.render("products/new", { categories });
 });
 // new should be before /:id because it can lead to express thinking new is an id and try to get the id from mongo which does not exist leading to crashing the server
 
@@ -37,7 +40,27 @@ app.get("/products/:id", async (req, res) => {
   // console.log(product);
   res.render("products/show", { product });
 });
-
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product, categories });
+});
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  // req.body has all the info that was changed using the form
+  // so we can use req.body to update the product
+  // console.log(req.body);
+  res.redirect(`/products/${product._id}`);
+});
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
 app.listen(3000, () => {
   console.log("Listening On Port 3000");
 });
